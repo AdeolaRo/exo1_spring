11dcd5249a0350f154c1800228c7be541e29978c
Abdel Chitou <char.wilsons@gmail.com>
2024-07-18 16:00:08 +0200

    commit 1

---
+++ src/test/java/org/example/exo1_spring/Exo1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.exo1_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exo1SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
